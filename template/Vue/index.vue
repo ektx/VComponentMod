c30c9927cd0a330bbb5e4b8177c18624a81d87c6
ektx <530675800@qq.com>
2019-05-17 13:44:58 +0800

    update

---
+++ template/Vue/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="my_component_mod">
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script src="./main.js"></script>
+<style src="./layout.less" lang="less" scoped></style>
